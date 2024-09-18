2f42ab7cd2d29cca7a23c3bc061e53dc9bd42078
dvyrj08 <you@example.com>
2024-09-18 19:34:21 -0400

    final

--- server.js
+++ server.js
@@ -68,6 +68,9 @@ const hbs = exphbs.create({
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 
+app.set('views', path.join(__dirname, 'views')); 
+app.set('view engine', 'ejs');
+
 
 // Route for "/about"
 app.get('/about', (req, res) => {
